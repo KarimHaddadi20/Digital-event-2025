43c15fd250972ec41b65322c22385d9a5004684c
Ash7gaut <antoningautier@outlook.fr>
2025-01-29 15:30:28 +0100

    changed title in loader

--- src/Js/Loader.js
+++ src/Js/Loader.js
@@ -64,7 +64,7 @@ class Loader {
 
             gsap.to(progress, {
                 value: 1,
-                duration: 1.3,
+                duration: 1,
                 onUpdate: () => {
                     const currentPercent = Math.round(progress.value * 100);
                     percentageElement.textContent = `${currentPercent}%`;
